786770fbaa4dab88d6c74a6300c802f79009faea
SudaraWishvajith <it21221378@my.sliit.lk>
2023-06-28 18:21:30 +0530

    implement ExpenseItem component
    In this i have import dispatch from Context, which allows to dispatch a delete action.

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -3,6 +3,28 @@ import { AppContext } from '../context/AppContext';
 import { FaTimesCircle } from 'react-icons/fa';
 
 const ExpenseItem = (props) => {
+
+    const { dispatch,Location } = useContext(AppContext);
+
+    const handleDeleteItem = ()=>{
+        const item = {
+            name: props.name,
+        };
+        dispatch({
+            type: 'DELETE_ITEM',
+            payload:item,
+        });
+    };
+
+    return(
+        <tr>
+            <td>{props.name}</td>
+            <td>{props.quantity}</td>
+            <td>{Location}{parseInt(props.unitPrice)}</td>
+            <td>{Location}{parseInt(props.unitPrice)*parseInt(props.quantity)}</td>
+            <td><FaTimesCircle size='2rem' color='red' onClick={handleDeleteItem}></FaTimesCircle></td>
+        </tr>
+    );
 };
 
 export default ExpenseItem;
