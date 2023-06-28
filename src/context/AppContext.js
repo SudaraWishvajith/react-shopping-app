8e4e95171db8115cdc20ca3058b523f7d1952af0
SudaraWishvajith <it21221378@my.sliit.lk>
2023-06-28 16:38:01 +0530

    implement AppContext
    this use for update the state, based on the action. this includes initial state for the Shopping Cart. Provider component which wraps the components want to give access to the state

--- src/context/AppContext.js
+++ src/context/AppContext.js
@@ -1,5 +1,61 @@
 import React, { createContext, useReducer } from 'react';
 
+// The reducer - this is used to update the state, based on the action
+export const AppReducer = (state, action) =>{
+    let new_expenses = [];
+    switch(action.type){
+        case 'ADD_QUANTITY':
+            let updatedqty = false;
+            state.expenses.map((expense) =>{
+                if(expense.name === action.payload.name) {
+                    expense.quantity = expense.quantity + action.payload.quantity;
+                    updatedqty = true;
+                }
+                new_expenses.push(expense);
+                return true;
+            })
+            state.expenses = new_expenses;
+            action.type = 'DONE';
+            return {
+                ...state,
+            };
+        
+        case 'RED_QUANTITY':
+            state.expenses.map((expense)=>{
+                if(expense.name === action.payload.name){
+                    expense.quantity = expense.quantity - payload.quantity;
+                }
+                new_expenses.push(expense);
+                return true;
+            })
+            state.expenses = new_expenses;
+            action.type = 'DONE';
+            return {
+                ...state,
+            }
+        case 'DELETE_ITEM':
+            state.expenses.map((expense)=>{
+                if(expense.name === action.payload.name){
+                    expense.quantity = 0;
+                }
+                new_expenses.push(expense);
+                return true;
+            })
+            state.expense = new_expenses;
+            action.type = 'DONE';
+            return {
+                ...state,
+            }
+        case 'CHG_LOCATION':
+            action.tyep = 'DONE';
+            state.Location = action.payload;
+            return {
+                ...state
+            }
+    }
+       
+};
+
 const initialState = {
     expenses: [
         { id: "Shirt", name: "Shirt", quantity: 0, unitPrice: 500 },
